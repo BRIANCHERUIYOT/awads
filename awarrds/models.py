565801dd340e404e7675b361ac814c024796293e
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-12-13 11:52:44 +0300

    adds profile model

--- awarrds/models.py
+++ awarrds/models.py
@@ -1,11 +1,27 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.utils import timezone
 from django.db.models.signals import post_save
 from tinymce.models import HTMLField
 from cloudinary.models import CloudinaryField
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = CloudinaryField('image')
+    bio = models.TextField(default="No bio!")
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
