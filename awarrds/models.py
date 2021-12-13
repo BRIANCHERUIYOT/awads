ad5426ca7cee5fdc957d46cfa0030753b9cdb9ac
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-12-13 11:36:09 +0300

    adds dependencies for models.py

--- awarrds/models.py
+++ awarrds/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.utils import timezone
+from django.db.models.signals import post_save
+from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
