90c7f563102808e65b762d28cf471ab15cd36b0d
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-12-14 09:45:26 +0300

    adds user.html rating.html projects.html edit_profile.html

--- awarrds/urls.py
+++ awarrds/urls.py
@@ -9,4 +9,5 @@ urlpatterns= [
     url(r'^new/post$', views.new_post, name='new_post'),
     url(r'^projects/$', views.project, name='project'),
     url(r'^accounts/profile/$', views.profile, name='profile'),
+    url(r'^accounts/profile/edit/$', views.edit, name='edit'),
 ]
